f674b655df698ce17ae8fdc7506ff8a5f6c1d9d8
Heathwang888 <yutai.asset@gmail.com>
2025-05-01 01:30:26 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,23 @@
+const CACHE_NAME = 'phone-contract-v1';
+const urlsToCache = [
+  '/',
+  '/index.html',
+  '/manifest.json',
+  '/images/page1.jpg',
+  '/images/page2.jpg',
+  '/images/page3.jpg'
+];
 
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
